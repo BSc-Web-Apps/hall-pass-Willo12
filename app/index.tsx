7869fa799b7dae382f4009d01409f856301a6f35
BrazlyBearWolf <merive33@gmail.com>
2025-03-27 16:05:16 +0000

    made basic tasks working with a tasks map

--- app/index.tsx
+++ app/index.tsx
@@ -8,11 +8,31 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import Logo from "~/components/ui/logo";
+import Task from "~/components/ui/task";
 import { Text } from "~/components/ui/text";
 
 export default function HomeScreen() {
+  const tasks = [
+    {
+      title: "task 1", category: "Category 1", isChecked: false, id: 1
+    },
+    {
+      title: "task 2", category: "Category 2", isChecked: false, id: 2
+    },
+    {
+      title: "task 3", category: "Category 3", isChecked: false, id: 3
+    },
+  ]
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-background">
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          category={task.category}
+          isChecked={task.isChecked}
+          title={task.title}
+        />
+      ))}
       <Logo />
     </View>
   );
---
+++ components/ui/task.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, } from "react-native";
+//import { Checkbox } from "~/components/ui/checkbox";
+
+interface TaskProps {
+  title: string;
+  category: string;
+  isChecked: boolean;
+}
+
+export default function Task({ title, category, isChecked }: TaskProps) {
+  const [checked, setChecked] = React.useState(false);
+  return (
+    <View>
+      <View>
+        {/* <Checkbox */}
+        {/*   checked={checked} */}
+        {/*   onCheckedChange={setChecked} */}
+        {/* /> */}
+      </View>
+      <View className="">
+        <Text className="text-white">{title}</Text>
+        <Text className="text-white">{category}</Text>
+      </View>
+    </View>
+  );
+}
