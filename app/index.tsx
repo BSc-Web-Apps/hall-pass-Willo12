e4cd708ba7bfb710ba7c6c36504203f85e4c64fd
Willo12 <WR114224@hc.ac.je>
2025-04-25 14:55:32 +0100

    made edit button work for tasks and added a cancel button for it

--- app/index.tsx
+++ app/index.tsx
@@ -13,70 +13,26 @@ import { Text } from "~/components/ui/text";
 
 export default function HomeScreen() {
 
-  const [tasks, setTasks] = React.useState({
-    task: [
-      {
-        title: "task 1", category: "Category 1", isChecked: false, id: 1
-      },
-      {
-        title: "task 2", category: "Category 2", isChecked: false, id: 2
-      },
-      {
-        title: "task 3", category: "Category 3", isChecked: false, id: 3
-      },
-    ]
-  });
-  const [newTitle, setNewTitle] = React.useState("");
-
-  function handleUpdateTask(event: any) {
-    event.preventDefault();
-    const nextTask = { ...tasks }
-
+  const [tasks, setTasks] = React.useState([
     {
-      nextTask.task.map((task) => (
-        task.title = newTitle
-      ))
-    }
+      title: "task 1", category: "Category 1", isChecked: false, id: 1
+    },
+    {
+      title: "task 2", category: "Category 2", isChecked: false, id: 2
+    },
+    {
+      title: "task 3", category: "Category 3", isChecked: false, id: 3
+    },
 
-    console.log(newTitle);
-    setTasks(nextTask);
-  }
+  ]);
+
+
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-background">
       <Logo />
-      {tasks.task.map((task) => (
-        <Task
-          key={task.id}
-          category={task.category}
-          isChecked={task.isChecked}
-          title={task.title}
-
-
-        />
-
+      {tasks.map((task) => (
+        <Task task={task} />
       ))}
-      <View>
-        <form onSubmit={handleUpdateTask}>
-          <View>
-
-            <label htmlFor="titleInput" className="text-white">
-              Title
-            </label>
-            <input
-              type="text"
-              value={newTitle}
-              onChange={(event) => setNewTitle(event.target.value)}
-            />
-
-          </View>
-          <View>
-            <button type="submit" className="text-white">submit</button>
-
-          </View>
-
-        </form>
-
-      </View>
 
     </View >
   );
--- components/ui/task.tsx
+++ components/ui/task.tsx
@@ -2,26 +2,77 @@ import React from "react";
 import { View, Text, } from "react-native";
 import { Checkbox } from "~/components/ui/checkbox";
 
-interface TaskProps {
+interface Task {
   title: string;
   category: string;
   isChecked: boolean;
 }
+interface TaskProps {
+  task: Task
+}
 
-export default function Task({ title, category, isChecked }: TaskProps) {
+export default function Task({ task }: TaskProps) {
   const [checked, setChecked] = React.useState(false);
+  const [stateTask, setStateTask] = React.useState(task);
+  const [isEditable, setIsEditable] = React.useState(false);
+  const [initialTask, setInitialTask] = React.useState(task);
+
+  const handleUpdateTask = (event: any) => {
+    event.preventDefault();
+    const updateTask = { ...stateTask }
+
+    setIsEditable(!isEditable);
+    setInitialTask(stateTask);
+    setStateTask(updateTask);
+  }
+
+  const handleCancelChanges = (event: any) => {
+    setIsEditable(!isEditable);
+    setStateTask(initialTask);
+  }
+
+  const handleTitleChange = (value: any) => {
+    const nextTask = { ...stateTask }
+    nextTask.title = value;
+
+    setStateTask(nextTask)
+  }
+
+  const handleCategoryChange = (value: any) => {
+    const nextTask = { ...stateTask }
+    nextTask.category = value;
+
+    setStateTask(nextTask)
+  }
   return (
-    <View className="flex-1 flex flex-row gap-2 justify-center items-center">
-      <View>
-        <Checkbox
-          checked={checked}
-          onCheckedChange={setChecked}
-        />
-      </View>
-      <View className="">
-        <Text className="text-white">{title}</Text>
-        <Text className="text-white">{category}</Text>
-      </View>
-    </View>
+    <>
+      <View className="flex-1 flex flex-row gap-2 justify-center items-center">
+        <View>
+          <Checkbox
+            checked={checked}
+            onCheckedChange={setChecked}
+          />
+        </View>
+        <View className={`${isEditable ? "flex" : "hidden"} flex-col gap-2 p-4 `}>
+          <input
+            type="text"
+            value={stateTask.title}
+            onChange={(event) => handleTitleChange(event.target.value)}
+          />
+          <input
+            type="text"
+            value={stateTask.category}
+            onChange={(event) => handleCategoryChange(event.target.value)}
+          />
+        </View>
+
+        <View className={`${isEditable ? "hidden" : "flex"} flex-col gap-2 p-4`}>
+          <Text className="text-white">{stateTask.title}</Text>
+          <Text className="text-white">{stateTask.category}</Text>
+        </View>
+        <button type="button" onClick={handleUpdateTask} className="mb-4 ml-4 px-4 py-1 rounded bg-gray-400 font-semibold text-gray-100 cursor-pointer hover:text-gray-300">{isEditable ? "Submit" : "Edit"}</button>
+        <button type="button" onClick={handleCancelChanges} className={`${isEditable ? "flex" : "hidden"} mb-4 ml-4 px-4 py-1 rounded bg-gray-400 font-semibold text-gray-100 cursor-pointer hover:text-gray-300`}>Cancel</button>
+      </View >
+    </>
   );
 }
